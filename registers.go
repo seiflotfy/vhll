862a6f90ce5e96ee3696b9957a9de3ec2bf9ef07
Seif Lotfy <seif.lotfy@gmail.com>
2015-08-28 16:10:33 +0200

    Add cardinality and offer functions

--- registers.go
+++ registers.go
@@ -7,11 +7,14 @@ type registerSet struct {
 }
 
 func newRegisterSet(count uint) *registerSet {
-	return &registerSet{Count: count}
+	r := &registerSet{Count: count, Size: count / 8}
+
+	r.reset()
+	return r
 }
 
 func newRegisterSetWithValues(count uint, initialValues []uint8) *registerSet {
-	rs := &registerSet{Count: count}
+	rs := &registerSet{Size: count / 8, Count: count}
 	rs.m = initialValues
 	rs.Size = uint(len(rs.m))
 	return rs
@@ -22,16 +25,16 @@ func (rs *registerSet) reset() {
 }
 
 func (rs *registerSet) set(pos uint, val uint8) {
-	rs.m[pos] = val
+	rs.m[pos/8] = val
 }
 
 func (rs *registerSet) get(pos uint) uint8 {
-	return rs.m[pos]
+	return rs.m[pos/8]
 }
 
 func (rs *registerSet) updateIfGreater(pos uint, val uint8) bool {
-	if rs.m[pos] < val {
-		rs.m[pos] = val
+	if rs.m[pos/8] < val {
+		rs.m[pos/8] = val
 		return true
 	}
 	return false
--- vhll.go
+++ vhll.go
@@ -1,11 +1,13 @@
 package vhll
 
 import (
-	"encoding/binary"
 	"errors"
+	"fmt"
+	"hllpp"
 	"math"
+	"strconv"
 
-	"github.com/spaolacci/murmur3"
+	"github.com/dgryski/go-spooky"
 )
 
 var mAlpha = []float64{
@@ -54,42 +56,56 @@ func getVirtualEstimatorSize(physicalLog2m uint) uint {
 }
 
 func getAlphaMM(log2m uint) float64 {
-	m := math.Pow(2, float64(log2m))
+	m := uint(math.Pow(2, float64(log2m)))
 
 	var alphaMM float64
 
 	// See the paper.
 	switch log2m {
 	case 4:
-		alphaMM = 0.673 * m * m
+		alphaMM = 0.673 * float64(m*m)
 		break
 	case 5:
-		alphaMM = 0.697 * m * m
+		alphaMM = 0.697 * float64(m*m)
 		break
 	case 6:
-		alphaMM = 0.709 * m * m
-		break
+		alphaMM = 0.709 * float64(m*m)
 	default:
-		alphaMM = (0.7213 / (1 + 1.079/m)) * m * m
+		alphaMM = (0.7213 / (1 + 1.079/float64(m))) * float64(m*m)
 	}
 
 	return alphaMM
+}
+
+func round(f float64) uint {
+	return uint(f + 0.5)
+}
+
+// Calculate the position of the leftmost 1-bit.
+func getLeadingZeros(val uint64, max uint32) uint8 {
+	r := uint32(1)
+	for val&0x8000000000000000 == 0 && r <= max {
+		r++
+		val <<= 1
+	}
+	return uint8(r)
 }
 
 /*
 VirtualHyperLogLog ...
 */
 type VirtualHyperLogLog struct {
-	registers       *registerSet
-	physicalLog2m   uint
-	physicalM       uint
-	physicalAlphaMM float64
-	virtualLog2m    uint
-	virtualM        uint
-	virtualAlphaMM  float64
-	virtualCa       float64
-	//hll           hll.HyperLogLog
-	murmur murmur3.Hash128
+	registers               *registerSet
+	physicalLog2m           uint
+	physicalM               uint
+	physicalAlphaMM         float64
+	virtualLog2m            uint
+	virtualM                uint
+	virtualAlphaMM          float64
+	virtualCa               float64
+	totalCardinalityCounter *hllpp.HLLPP
+	totalCardinality        int64
+	noiseCorrector          float64
 }
 
 /*
@@ -114,7 +130,9 @@ New ...
 func New(physicalLog2m uint, registers *registerSet) (*VirtualHyperLogLog, error) {
 	vhll := &VirtualHyperLogLog{}
 	vhll.registers = registers
+	fmt.Println(len(registers.m))
 	vhll.physicalLog2m = physicalLog2m
+	vhll.physicalAlphaMM = getAlphaMM(physicalLog2m)
 	vhll.physicalM = uint(math.Pow(2, float64(physicalLog2m)))
 
 	if physicalLog2m < 7 {
@@ -122,10 +140,13 @@ func New(physicalLog2m uint, registers *registerSet) (*VirtualHyperLogLog, error
 	}
 
 	vhll.virtualLog2m = getVirtualEstimatorSize(physicalLog2m)
-	vhll.virtualAlphaMM = getAlphaMM(physicalLog2m)
+	vhll.virtualAlphaMM = getAlphaMM(vhll.virtualLog2m)
+
 	vhll.virtualM = uint(math.Pow(2, float64(vhll.virtualLog2m)))
 	vhll.virtualCa = mAlpha[vhll.virtualLog2m]
-	vhll.murmur = murmur3.New128()
+	vhll.totalCardinality = -1
+	vhll.noiseCorrector = 1
+	vhll.totalCardinalityCounter = hllpp.New()
 	return vhll, nil
 }
 
@@ -136,47 +157,111 @@ func (vhll *VirtualHyperLogLog) Reset() {
 	vhll.registers.reset()
 }
 
-/*
-GetRegisterValueDistribution ...
-*/
-func (vhll *VirtualHyperLogLog) GetRegisterValueDistribution(dist []float64) error {
-	if dist == nil || len(dist) < 32 {
-		return errors.New("distribution can't be < 32")
-	}
-	dist = make([]float64, len(dist), len(dist))
-	for i := uint(0); i < vhll.physicalM; i++ {
-		val := vhll.registers.get(i)
-		dist[val]++
-	}
-	for i, val := range dist {
-		dist[i] = val / float64(vhll.virtualM)
-	}
-	return nil
+func (vhll *VirtualHyperLogLog) getPhysicalRegisterFromVirtualRegister(counterIdx []byte, virtual uint) uint {
+	idx := uint(spooky.Hash64(counterIdx))
+	n := (idx+13)*104729 + virtual
+	h1 := spooky.Hash64([]byte(strconv.Itoa(int(n))))
+	return uint((uint(h1) & 0xFFFFFFFFFFFF) % vhll.physicalM)
 }
 
 /*
-GetRegisterValueDistributionForIdx ...
+OfferHashed ...
 */
-func (vhll *VirtualHyperLogLog) GetRegisterValueDistributionForIdx(counterIdx uint, dist []float64) error {
-	if dist == nil || len(dist) < 32 {
-		return errors.New("distribution can't be < 32")
-	}
-	dist = make([]float64, len(dist), len(dist))
-	for i := uint(0); i < vhll.virtualM; i++ {
-		phyIdx := vhll.physicalRegisterFromVirtualRegister(counterIdx, i)
-		val := vhll.registers.get(phyIdx)
-		dist[val]++
-	}
-	for i, val := range dist {
-		dist[i] = val / float64(vhll.virtualM)
-	}
-	return nil
+func (vhll *VirtualHyperLogLog) OfferHashed(counterIdx []byte, hashedValue uint64) bool {
+	virtualRegister := hashedValue >> (64 - vhll.virtualLog2m)
+	r := getLeadingZeros(((hashedValue<<vhll.virtualLog2m)|(1<<(vhll.virtualLog2m-1))+1)+1, 32)
+	physicalRegister := vhll.getPhysicalRegisterFromVirtualRegister(counterIdx, uint(virtualRegister))
+	return vhll.registers.updateIfGreater(physicalRegister, r)
 }
 
-func (vhll *VirtualHyperLogLog) physicalRegisterFromVirtualRegister(counterIdx uint, virtual uint) uint {
-	n := (counterIdx+13)*104729 + virtual
-	b := make([]byte, 64)
-	binary.LittleEndian.PutUint64(b, uint64(n))
-	hash := murmur3.Sum64(b)
-	return uint((uint(hash) & 0xFFFFFFFFFFFF) % vhll.physicalM)
+/*
+Offer ...
+*/
+func (vhll *VirtualHyperLogLog) Offer(counterIdx []byte, data []byte) bool {
+	vhll.totalCardinality = -1
+	data = append(data, counterIdx...)
+	h1 := spooky.Hash64(data)
+	vhll.totalCardinalityCounter.Add(data)
+	return vhll.OfferHashed(counterIdx, h1)
+}
+
+/*
+GetTotalCardinality ...
+*/
+func (vhll *VirtualHyperLogLog) GetTotalCardinality() uint64 {
+
+	if vhll.totalCardinality >= 0 {
+		return uint64(vhll.totalCardinality)
+	}
+	vhll.totalCardinality = int64(vhll.totalCardinalityCounter.Count()) * 2
+	fmt.Println(">>>> totalCardinality", vhll.totalCardinality)
+
+	registerSum := float64(0)
+	count := vhll.registers.Count
+	zeros := 0.0
+
+	totalCardinalityFromPhySpace := 0
+	for j := uint(0); j < count; j++ {
+		val := vhll.registers.get(j)
+		registerSum += 1.0 / float64(uint(1)<<val)
+		if val == 0 {
+			zeros++
+		}
+	}
+
+	estimate := vhll.physicalAlphaMM * (1 / registerSum)
+	if estimate <= (5.0/2.0)*float64(count) {
+		totalCardinalityFromPhySpace = int(round(float64(count) * math.Log(float64(count)/zeros)))
+	} else {
+		totalCardinalityFromPhySpace = int(round(estimate))
+	}
+
+	vhll.noiseCorrector = 1.0 * float64(vhll.totalCardinality) / float64(totalCardinalityFromPhySpace)
+	vhll.totalCardinality = int64(round(float64(totalCardinalityFromPhySpace)))
+	return uint64(vhll.totalCardinality)
+}
+
+/*
+GetNoiseMean ...
+*/
+func (vhll *VirtualHyperLogLog) GetNoiseMean() float64 {
+	nhat := vhll.GetTotalCardinality()
+	m := vhll.physicalM
+	s := vhll.virtualM
+	return float64(uint(nhat)) * float64(s/m)
+}
+
+/*
+GetCardinality ...
+*/
+func (vhll *VirtualHyperLogLog) GetCardinality(counterIdx []byte) float64 {
+
+	physicalCardinality := vhll.GetTotalCardinality()
+	registerSum := float64(0)
+	zeros := float64(0)
+	for j := uint(0); j < vhll.virtualM; j++ {
+		phyReg := vhll.getPhysicalRegisterFromVirtualRegister(counterIdx, j)
+		val := vhll.registers.get(phyReg)
+		registerSum += 1.0 / float64(uint(1)<<val)
+		if val == 0 {
+			zeros++
+		}
+	}
+	estimate := float64(vhll.virtualAlphaMM) * (1 / registerSum)
+
+	virtualCardinality := float64(round(estimate))
+
+	vp := float64(1.0 * vhll.physicalM * vhll.virtualM / (vhll.physicalM - vhll.virtualM))
+	result := float64(0)
+	noiseMean := vhll.GetNoiseMean()
+
+	if vhll.virtualLog2m >= vhll.physicalLog2m-6 {
+		result = float64(round(vp * ((virtualCardinality)/float64(vhll.virtualM) - float64(physicalCardinality/uint64(vhll.physicalM)))))
+	} else {
+		result = float64(round(vp * ((virtualCardinality)/float64(vhll.virtualM) - float64(physicalCardinality)/vhll.noiseCorrector/float64(vhll.physicalM))))
+		if result-(1.2*noiseMean) > 0 {
+			result = float64(round(vp * ((virtualCardinality)/float64(vhll.virtualM) - float64(physicalCardinality/uint64(vhll.physicalM)))))
+		}
+	}
+	return result
 }
